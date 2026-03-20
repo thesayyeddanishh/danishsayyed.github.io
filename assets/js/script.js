e049f5d2b21cb1891fbd599eae5c5d1af446f24c
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-03-20 21:20:43 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,3 +181,42 @@ filterBtns.forEach(btn => {
 
   });
 });
+
+/* ================= TESTIMONIAL MODAL ================= */
+
+const testimonialModal = document.querySelector("[data-modal-container]");
+const testimonialOverlay = document.querySelector("[data-overlay]");
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+const modalRole = document.querySelector("[data-modal-role]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+
+testimonialsItem.forEach(item => {
+  item.addEventListener("click", function () {
+
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    const role = this.querySelector(".testimonials-role");
+    modalRole.innerHTML = role ? role.innerHTML : "";
+
+    testimonialModal.classList.add("active");
+    testimonialOverlay.classList.add("active");
+  });
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  testimonialModal.classList.remove("active");
+  testimonialOverlay.classList.remove("active");
+});
+
+testimonialOverlay.addEventListener("click", () => {
+  testimonialModal.classList.remove("active");
+  testimonialOverlay.classList.remove("active");
+});
